3d3b9598a6010cced23497b430cd8308ec78463f
kilsonrs <kilsonrs@gmail.com>
2018-11-26 16:18:24 -0200

    Autenticacao

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,9 +5,9 @@ class UserController {
     return res.render('auth/signup')
   }
   async store (req, res) {
-    req.body.avatar = 'teste.jpg'
+    const { filename: avatar } = req.file
 
-    await User.create(req.body)
+    await User.create({ ...req.body, avatar })
 
     return res.redirect('/')
   }
